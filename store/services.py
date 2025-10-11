44ec85f7842cca63df36a0ed62e8aaa809f75163
Bruno V <bruno.vn.lima@gmail.com>
2025-10-10 22:20:05 -0300

    Added more status messages to be sent via email

--- store/services.py
+++ store/services.py
@@ -44,6 +44,11 @@ def verify_order_status(status, email, order):
         "pending":  send_email_message.order_created,
         "paid": send_email_message.paid_order, 
         "processing": send_email_message.processing_order,
+        "shipped": send_email_message.order_shipped,
+        "out_for_delivery": send_email_message.order_out_for_delivery,
+        "delivered": send_email_message.order_delivered,
+        "cancelled": send_email_message.order_cancelled, 
+        "refunded": send_email_message.refunded_order
     }
 
     method = status_methods.get(status)
@@ -60,10 +65,10 @@ def verify_order_status(status, email, order):
 #         "out_for_delivery": "Out for delivery",
 #         "delivered": "Delivered",
 #         "cancelled": "Cancelled",
-#         "refunded": "Refunded",
+#         "refunded": "Refunded",                                               
 # }
 
-class EmailService:
+class EmailService:                                                             #ARRUMAR O INGLES
     def __init__(self, email, order):
         self.order_code = str(order.order_id)[:7]
         self.email = email
@@ -71,7 +76,7 @@ class EmailService:
     def order_created(self):
         email_message = EmailMessage(
             subject = f"Your order #{self.order_code} has been created successfully!",
-            body = f"Your order #{self.order_code} has been created. You'll be notified as soon as there are updates.",
+            body = f"Your order #{self.order_code} has been created. You'll be notified as soon as there are any updates.",
             to = [self.email]
         )
         email_message.send()
@@ -79,15 +84,54 @@ class EmailService:
     def processing_order(self):
         email_message = EmailMessage(
             subject = f"Your order #{self.order_code} is being processed!",
-            body = f"We'd like to inform you that your order #{self.order_code} is being processed. As soon as there are updates, you'll be notified.",
+            body = f"We'd like to inform you that your order #{self.order_code} is being processed. You'll be notified as soon as there are any updates.",
             to = [self.email]
         )
         email_message.send()
 
     def paid_order(self):
         email_message = EmailMessage(
-            subject = f"Your order payment has been approved!",
-            body = f"We'd like to inform you that your payment has been approved! As soon as there are updates about your order #{self.order_code}, you'll be notified.",
+            subject = f"The payment for your order #{self.order_code} has been approved!",
+            body = f"We'd like to inform you that your payment has been approved! As soon as there are updates regarding your order #{self.order_code}, you'll be notified.",
             to = [self.email]
+        )
+        email_message.send()
+
+    def order_shipped(self):
+        email_message = EmailMessage(
+            subject = f"Your order #{self.order_code} has been shipped!",
+            body = f"We'd like to inform you that your order #{self.order_code} has been shipped. You'll receive further updates about your order.",
+            to = [self.email]
+        )
+        email_message.send()
+
+    def order_out_for_delivery(self):
+        email_message = EmailMessage(
+            subject = f"Your order #{self.order_code} has left for delivery!",
+            body = f"We'd like to inform you that your order #{self.order_code} has left for delivery. It will arrive at your residence soon.",
+            to = [self.email]
+        )
+        email_message.send()
+
+    def order_delivered(self):
+        email_message = EmailMessage(
+            subject = f"Your order #{self.order_code} has been delivered successfully!",
+            body = f"Your order #{self.order_code} has been delivered successfully. Thank you for trusting us. Enjoy your product!",
+            to = [self.email]
+        )
+        email_message.send()
+    
+    def order_cancelled(self):
+        email_message = EmailMessage(
+            subject = f'Your order #{self.order_code} has been cancelled successfully!',
+            body = f"We'd like to inform you that your order has been cancelled successfully. You'll be notified if any further action is required.",
+            to = [self.email]
+        )
+        email_message.send()
+
+    def refunded_order(self):
+        email_message = EmailMessage(
+            subject = f"Your order #{self.order_code} has been refunded successfully!",
+            body = f"We'd like to inform you that your order #{self.order_code} has been refunded successfully."
         )
         email_message.send()
